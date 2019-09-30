d5c3e88bd4dfe9bc912f703b0ab3ab3e9151d0f1
Brianno Coller <bdcoller@gmail.com>
2019-09-29 23:10:20 -0500

    initial commit for main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// main.cpp
+//
+// Program for testing Euler integrator.
+//
+// Your Name Here
+//============================================================================
+
+#include <iostream>
+//#include "eulerStep.h"
+
+void rhs(double* f, double* x, double t);
+
+using namespace std;
+
+int main()
+{
+	cout << "It works." << endl;
+
+	return(0);
+}
